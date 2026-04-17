1d39f1381ecff30bc4ed8bcf5ea49d25c261b5b7
G1RAAJ <jeevanraju.ch@gmail.com>
2026-04-17 20:27:10 +0530

    Update Jenkins_Script.sh

--- DevOps_Project_3/Jenkins_Script.sh
+++ DevOps_Project_3/Jenkins_Script.sh
@@ -1,40 +1,48 @@
 #!/bin/bash
-set -ex
+set -euo pipefail
+set -x
 
+# Export Terraform variables
+export TF_VAR_region="${REGION}"
+export TF_VAR_vpc_id="${VPC_ID}"
+export TF_VAR_cluster_name="${CLUSTER_NAME}"
 
-export TF_VAR_region=$REGION
-export TF_VAR_vpc_id=$VPC_ID
-export TF_VAR_cluster_name=$CLUSTER_NAME
+# Go to Terraform directory
+cd "${WORKSPACE}/DevOps_Project_3/Terraform"
 
-#goint to the terraform path
-cd ${WORKSPACE}/DevOps_Project_3/Terraform
+# Replace cluster name safely in backend.tf
+sed -i "s|ngg_cluster_name|${CLUSTER_NAME}|g" backend.tf
 
-#replacing the cluster name field in backend.tf file
-sed -i "s/ngg_cluster_name/$CLUSTER_NAME/g" backend.tf
-
-#running terraform command
-terraform init    
+# Run Terraform commands
+terraform init
 terraform plan
-terraform $ACTION --auto-approve
+terraform "${ACTION}" --auto-approve
 
-if [ $ACTION == "apply" ]; then
-#login into the eks cluster
-aws eks update-kubeconfig --name $CLUSTER_NAME --region $REGION
-kubectl get pods -A
+# If action is apply
+if [ "${ACTION}" = "apply" ]; then
+  # Verify AWS access
+  aws sts get-caller-identity
 
-#installing bitnami helm chart
-helm repo add bitnami https://charts.bitnami.com/bitnami
+  # Configure kubectl for EKS
+  aws eks update-kubeconfig --name "${CLUSTER_NAME}" --region "${REGION}"
 
-#installing eks helm chart
-helm repo add eks https://aws.github.io/eks-charts
+  # Verify cluster connectivity
+  kubectl get nodes
 
-#installing nginx
-helm upgrade --install nginx bitnami/nginx
+  # Add Helm repositories
+  helm repo add bitnami https://charts.bitnami.com/bitnami || true
+  helm repo add eks https://aws.github.io/eks-charts || true
 
-#installing aws loadbalancer controller
-helm upgrade --install lb-controller eks/aws-load-balancer-controller --set clusterName=$CLUSTER_NAME
+  # Update Helm repos
+  helm repo update
 
+  # Install Nginx
+  helm upgrade --install nginx bitnami/nginx
+
+  # Install AWS Load Balancer Controller
+  helm upgrade --install lb-controller eks/aws-load-balancer-controller \
+    --set clusterName="${CLUSTER_NAME}"
 
 else
-echo "no need to install"
+  echo "Skipping Helm deployment (ACTION=${ACTION})"
 fi
