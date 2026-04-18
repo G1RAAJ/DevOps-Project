a06c84fd5b439456b5dd54827a69ca0efc190883
G1RAAJ <jeevanraju.ch@gmail.com>
2026-04-18 13:21:47 +0530

    Update providers.tf

--- DevOps_Project_3/Terraform/providers.tf
+++ DevOps_Project_3/Terraform/providers.tf
@@ -11,12 +11,6 @@ terraform {
       version = "~> 3.0"
     }
   }
-
-  backend "s3" {
-    bucket = "amzn-ec2-eks"   # CHANGE
-    key    = "eks/terraform.tfstate"
-    region = "us-east-1"
-  }
 }
 
 provider "aws" {
