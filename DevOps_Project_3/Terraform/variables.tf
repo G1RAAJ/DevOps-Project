5d5f0d4dc03a1da3ee606e9fce3d0f8abaf21a3c
G1RAAJ <jeevanraju.ch@gmail.com>
2026-04-18 14:53:46 +0530

    Update variables.tf

--- DevOps_Project_3/Terraform/variables.tf
+++ DevOps_Project_3/Terraform/variables.tf
@@ -1,3 +1,19 @@
-variable "region" {}
-variable "vpc_id" {}
-variable "cluster_name" {}
+variable "region" {
+    default = ""
+}
+
+
+
+variable "vpc_id" {
+    default = ""
+}   
+
+variable "cluster_name" {
+    default = ""
+}
+
+
+
+variable "node_instance_type" {
+    default = "c5.xlarge"
+}
