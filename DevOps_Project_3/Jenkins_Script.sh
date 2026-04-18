ba906456205f58f2557e1d827de2199ae1a17ba2
G1RAAJ <jeevanraju.ch@gmail.com>
2026-04-18 13:55:09 +0530

    Update Jenkins_Script.sh

--- DevOps_Project_3/Jenkins_Script.sh
+++ DevOps_Project_3/Jenkins_Script.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
-set -euo pipefail
+set -eo pipefail
 set -x
+
+# ✅ Default value if not passed
+ACTION=${ACTION:-apply}
 
 export TF_VAR_region="${REGION}"
 export TF_VAR_vpc_id="${VPC_ID}"
 export TF_VAR_cluster_name="${CLUSTER_NAME}"
 
-cd ${WORKSPACE}/DevOps_Project_3/Terraform
+cd "${WORKSPACE}/DevOps_Project_3/Terraform"
 
 terraform init -reconfigure
 terraform plan
 terraform ${ACTION} -auto-approve
 
 if [ "${ACTION}" = "apply" ]; then
-
   aws eks update-kubeconfig --name ${CLUSTER_NAME} --region ${REGION}
-
   kubectl get nodes
 
   helm repo add bitnami https://charts.bitnami.com/bitnami || true
   helm repo update
 
   kubectl create namespace nginx || true
-
   helm upgrade --install nginx bitnami/nginx -n nginx
-
-  kubectl get svc -n nginx
-
-else
-  echo "Destroy completed"
 fi
